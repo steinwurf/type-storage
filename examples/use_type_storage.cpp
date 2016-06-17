89ccd00e90491a8a2d35d218340b0469cdca147c
Jeppe Krigslund <krigslund@steinwurf.com>
2016-06-17 09:46:34 +0200

    added baget func and helpers, and example

--- examples/use_type_storage.cpp
+++ examples/use_type_storage.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <typeinfo>
 #include <iostream>
+#include <string>
 
 #include <type-storage/type_storage.hpp>
 
@@ -19,29 +20,72 @@ std::tuple<int32_t, uint32_t, float> sums;
 template<typename T>
 void add(T t)
 {
-    type_storage::get<T>(sums)+=t;
+	type_storage::get<T>(sums)+=t;
 }
+
+// Base class with base functions, virtual and non-virtual
+struct base
+{
+	void print()
+	{
+		std::cout << "print() from 'base'" << std::endl;
+	}
+
+	virtual void vprint()
+	{
+		std::cout << "vprint() from 'base'" << std::endl;
+	}
+};
+
+// Derived class with overridden functions, both virtual and non-virtual
+struct derived : public base
+{
+	void print()
+	{
+		std::cout << "print() from 'derived'" << std::endl;
+	}
+
+	virtual void vprint()
+	{
+		std::cout << "vprint() from 'derived'" << std::endl;
+	}
+};
+
+
+
 
 int main()
 {
-    // Create variables of different types to add with function 'add'
-    int32_t a = -5;
-    uint32_t b = 10U;
-    float c = 3.14f;
+	// Create variables of different types to add with function 'add'
+	int32_t a = -5;
+	uint32_t b = 10U;
+	float c = 3.14f;
 
-    // Add the values a couple of times.
-    add(a);
-    add(a);
-    add(b);
-    add(b);
-    add(c);
-    add(c);
+	// Add the values a couple of times.
+	add(a);
+	add(a);
+	add(b);
+	add(b);
+	add(c);
+	add(c);
 
-    // The sums can now be tracked using a type, and thus only using one
-    // variable (sums) to keep track of an arbitrary amount of sums.
-    std::cout << "sums are: \n"
-              << "int32_t:  \t" << type_storage::get<int32_t>(sums) << "\n"
-              << "uint32_t:  \t" << type_storage::get<uint32_t>(sums) << "\n"
-              << "float:  \t" << type_storage::get<float>(sums)
-              << std::endl;
+	// The sums can now be tracked using a type, and thus only using one
+	// variable (sums) to keep track of an arbitrary amount of sums.
+	std::cout << "sums are: \n"
+						<< "int32_t:  \t" << type_storage::get<int32_t>(sums) << "\n"
+						<< "uint32_t:  \t" << type_storage::get<uint32_t>(sums) << "\n"
+						<< "float:  \t" << type_storage::get<float>(sums)
+						<< std::endl;
+
+	//--- typestorage::baget<B>() example
+
+    // Container with different types, some derived from a base
+    std::tuple<std::string, uint32_t, float, char, std::tuple<int>, derived> t;
+
+    // With type-storage::baget we fetch the Base-type object of a derived type
+    // in the tuple. NB: The original object stored in the tuple is a derived
+    // type, e.g. virtual functions in the base may have been overridden.
+    base& baze = type_storage::baget<base>(t);
+    baze.print(); // calls base::print()
+    baze.vprint(); // calls derived::vprint()
 }
--- src/type-storage/type_storage.hpp
+++ src/type-storage/type_storage.hpp
@@ -51,6 +51,49 @@ namespace type_storage
             static const size_t value =
                 find_type<false, U, Ts...>::template index <Ts...>::value;
         };
+
+
+        /// Helper struct for find_base_index. "Empty" non specialised struct.
+        template<bool cond, typename... Ts>
+        struct find_base
+        {
+            static_assert(cond, "Base type not found in provided typelist");
+        };
+
+        /// Helper struct for find_base_index, true specialisation.
+        /// template member index<>::value finds index based on the size of
+        /// remaining paramter pack Ts... when a type match has been found.
+        template<typename... Ts>
+        struct find_base<true, Ts...>
+        {
+            /// specifies the index of the found base type
+            template<typename... Types>
+            struct index
+            {
+                /// describes the index of requested type in Ts...
+                static const size_t value = sizeof...(Types) - sizeof...(Ts);
+            };
+        };
+
+        /// Helper struct for find_base_index, false specialization.
+        /// Check for type match between U and T, and inherits from correct
+        /// find_type specialisation. member index<>::value obtained by
+        /// recursion (recurses until U == T )
+        template<typename B, typename T, typename... Ts>
+        struct find_base<false, B, T, Ts...>
+            : find_base<std::is_base_of<B, T>::value, B, Ts...>
+        {};
+
+
+        /// Finds index of type U in template parameter pack Ts using find_type
+        template<typename B, typename... Ts>
+        struct find_base_index
+        {
+            /// describes the index of U in Ts...
+            static const size_t value =
+                find_base<false, B, Ts...>::template index <Ts...>::value;
+        };
+
     }
 
     /// Get a object of specific type T from tuple regardless of its position.
@@ -71,6 +114,26 @@ namespace type_storage
     const T& get(const std::tuple<Types...>& tup)
     {
         return std::get<detail::find_index<T, Types...>::value>(tup);
+    }
+
+    /// Get a object of Base type B from tuple regardless of its position.
+    /// This function is virtually identical to get(), however the matching
+    /// condition is on base types, not exact types.
+    /// NB: If tuple contains more than one object of a matching base type,
+    /// a reference to the first found object with base type B is returned.
+    /// If base type B is not contained in tuple the code cannot compile.
+    /// @param tup the tuple of objects to look in
+    template<typename B, typename... Types>
+    B& baget(std::tuple<Types...>& tup)
+    {
+        return std::get<detail::find_base_index<B, Types...>::value>(tup);
+    }
+
+    // Const version of above
+    template<typename B, typename... Types>
+    const B& baget(const std::tuple<Types...>& tup)
+    {
+        return std::get<detail::find_base_index<B, Types...>::value>(tup);
     }
 }
 
